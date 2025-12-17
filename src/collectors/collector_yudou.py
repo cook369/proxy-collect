3e0e41434ed4141fd3d1db5af819f75d70cf97e1
luoye2025 <luoye2021@qq.com>
2025-12-17 12:47:40 +0800

    fix: collect miss

--- src/collectors/collector_yudou.py
+++ src/collectors/collector_yudou.py
@@ -12,7 +12,7 @@ from .base import BaseCollector, register_collector
 @register_collector
 class CollectorYudou(BaseCollector):
     name = "yudou"
-    home_page = "https://www.yudou123.top/"
+    home_page = "https://www.yudou6677.top"
     AES_PATTERN = r"U2FsdGVkX1[0-9A-Za-z+/=]+"
     PASSWORD_RANGE = (1000, 9999)
 
